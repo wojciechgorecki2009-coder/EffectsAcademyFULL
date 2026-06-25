16bdfa7a506070d1edb9b7605fa7bf3bbb01a2ac
wojciechgorecki2009-coder <wojciechgorecki2009@gmail.com>
2026-06-25 13:02:33 +0100

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "https://effects-academy-api.onrender.com";
 export const API = `${BACKEND_URL}/api`;
 export const FILE_BASE = BACKEND_URL;
 
